cd4a9ab425539340caa169a39e13245d1b52c921
thrinath24 <thrinathsai1995@gmail.com>
2017-02-15 14:48:47 +0530

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -1,24 +1,11 @@
-/*
-Copyright IBM Corp 2016 All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 
 package main
 
 import (
 	"errors"
 	"fmt"
+	"encoding/json"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -26,6 +13,26 @@ import (
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
 }
+
+
+type MilkContainer struct{
+
+        ContainerID string `json:"containerid"`
+        User string        `json:"user"`
+
+        Litres string        `json:"litres"`
+
+}
+
+
+type SupplyCoin struct{
+
+        CoinID string `json:"coinid"`
+        User string        `json:"user"`
+}
+
+
+
 
 // ============================================================================================================================
 // Main
@@ -39,10 +46,17 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	var err error
+
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
-	}
+                                                          
+}
 
+       err = stub.PutState("hello world",[]byte(args[0]))  //Just to check the network 
+       if err != nil {
+		return nil, err
+}
 	return nil, nil
 }
 
@@ -53,10 +67,48 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	}else if function == "Create_milkcontainer" {		//creates a milk container-invoked by supplier   
+		return t.Create_milkcontainer(stub, args)      
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+
+
+func (t *SimpleChaincode) Create_milkcontainer(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+var err error
+
+// "1x22" "supplier" 20 
+// args[0] args[1] args[2] 
+
+id := args[0]
+user := args[1]
+litres :=args[2] 
+milkAsBytes, err := stub.GetState(id) 
+if err != nil {
+		return nil, errors.New("Failed to get details og given id") 
+}
+
+res := MilkContainer{} 
+json.Unmarshal(milkAsBytes, &res)
+
+if res.ContainerID == id{
+
+        fmt.Println("Container already exixts")
+        fmt.Println(res)
+        return nil,errors.New("This cpontainer alreadt exists")
+}
+
+res.ContainerID = id
+res.User = user
+res.Litres = litres
+milkAsBytes, _ =json.Marshal(res)
+
+stub.PutState(id,milkAsBytes)
+return nil,nil
+
 }
 
 // Query is our entry point for queries
@@ -64,11 +116,29 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "dummy_query" {											//read a variable
-		fmt.Println("hi there " + function)						//error
-		return nil, nil;
+	if function == "read" { //read a variable
+		return t.read(stub, args)
 	}
-	fmt.Println("query did not find func: " + function)						//error
+	fmt.Println("query did not find func: " + function)
 
 	return nil, errors.New("Received unknown function query: " + function)
+}
+
+
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
 }
