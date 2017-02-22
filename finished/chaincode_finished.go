098ad23612f14977c5ba34b53353a27f741b6bc8
thrinath24 <thrinathsai1995@gmail.com>
2017-02-23 00:15:41 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -19,12 +19,25 @@ package main
 import (
 	"errors"
 	"fmt"
+	"encoding/json"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
+}
+
+var containerIndexStr = "_containerindex"    //This will be used as key and a value will be an array of Container IDs	
+
+
+type MilkContainer struct{
+
+        ContainerID string `json:"containerid"`
+        User string        `json:"user"`
+
+        Litres string        `json:"litres"`
+
 }
 
 func main() {
@@ -36,16 +49,29 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
+	
+	
+	var err error
+
+       if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
-	}
+       }
 
-	err := stub.PutState("hello_world", []byte(args[0]))
-	if err != nil {
+       err = stub.PutState("hello world",[]byte(args[0]))  //Just to check the network whether we can read and write
+       if err != nil {
 		return nil, err
-	}
+       }
+	
+        /* Reset container index list - Making sure the value corresponding to containerIndexStr  is empty */
 
-	return nil, nil
+       var empty []string
+       jsonAsBytes, _ := json.Marshal(empty)                                   //create an empty array of string
+       err = stub.PutState(containerIndexStr, jsonAsBytes)                     //Resetting - Making milk container list as empty 
+       if err != nil {
+		return nil, err
+        }  
+	
+	 return nil, nil
 }
 
 // Invoke isur entry point to invoke a chaincode function
@@ -57,12 +83,85 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
+	}else if function == "Create_milkcontainer" {		//creates a milk container-invoked by supplier   
+		res,err := t.Create_milkcontainer(stub, args)
+		printdetails(stub, 3)
+		return res,err
 	}
 	fmt.Println("invoke did not find func: " + function)
 
 	return nil, errors.New("Received unknown function invocation: " + function)
 }
 
+
+
+
+
+
+func (t *SimpleChaincode) Create_milkcontainer(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+var err error
+
+// "1x22" "supplier" 20 
+// args[0] args[1] args[2] 
+
+id := args[0]
+user := args[1]
+litres :=args[2] 
+	
+// Checking if the container already exists in the network
+milkAsBytes, err := stub.GetState(id) 
+if err != nil {
+		return nil, errors.New("Failed to get details og given id") 
+}
+
+res := MilkContainer{} 
+json.Unmarshal(milkAsBytes, &res)
+
+if res.ContainerID == id{
+
+        fmt.Println("Container already exixts")
+        fmt.Println(res)
+        return nil,errors.New("This cpontainer alreadt exists")
+}
+
+//If not present, create it and Update ledger, containerIndexStr, Assets of Supplier
+//Creation
+res.ContainerID = id
+res.User = user
+res.Litres = litres
+milkAsBytes, _ =json.Marshal(res)
+
+stub.PutState(res.ContainerID,milkAsBytes)
+	
+//Update containerIndexStr	
+	containerAsBytes, err := stub.GetState(containerIndexStr)
+	if err != nil {
+		return nil, errors.New("Failed to get container index")
+	}
+	var containerIndex []string                                        //an array to store container indices - later this wil be the value for containerIndexStr
+	json.Unmarshal(containerAsBytes, &containerIndex)	
+	
+	
+	containerIndex = append(containerIndex, res.ContainerID)          //append the newly created container to the global container list									//add marble name to index list
+	fmt.Println("! container index: ", containerIndex)
+	jsonAsBytes, _ := json.Marshal(containerIndex)
+        err = stub.PutState(containerIndexStr, jsonAsBytes)
+	 
+
+	return nil,nil
+
+}
+
+func printdetails(stub  shim.ChaincodeStubInterface, a int)(err error) {
+
+	var  err error
+	fmt.Println(a)
+	err = stub.PutState("hello cts",[]byte("Inside printdetails")  //Just to check the network whether we can read and write
+       if err != nil {
+		return nil, err
+       }
+
+}
 // Query is our entry point for queries
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
