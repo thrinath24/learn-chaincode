0b43d77b3fc1600f724cddb3377ad247ad4dd7de
thrinath24 <thrinathsai1995@gmail.com>
2017-02-28 15:46:23 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -65,7 +65,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	
 	var err error
 	
-	fmt.Println("Welcome to Supplychain management Phase 1, Deployment is on the go")
+	fmt.Println("Welcome to the Supplychain management Phase 1, Deployment is on the go")
  
        if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
