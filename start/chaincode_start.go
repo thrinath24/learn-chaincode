cc942a125e6bb61eb9c241f8020472a6e6ee0920
thrinath24 <thrinathsai1995@gmail.com>
2017-03-07 16:03:20 +0530

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -72,9 +72,11 @@ type Asset struct{
 
 func main() {
 	err := shim.Start(new(SimpleChaincode))
+	
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
+	fmt.Printf("every time we enter main function")
 }
 
 // Init resets all the things
