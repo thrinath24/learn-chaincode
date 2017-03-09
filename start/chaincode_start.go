694e9f6a4b57281b3e38b33ea4043bbbddf87099
thrinath24 <thrinathsai1995@gmail.com>
2017-03-09 15:51:45 +0530

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -87,7 +87,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	
 	fmt.Println("Welcome to  Supply chain management , Deployment has been started, do as u want")
 	fmt.Printf("Suuuuper flow it is")
-	fmt.Printf("Hope this will shine in futurre")
+	fmt.Printf("Hope this will shine in future")
 	
  
        if len(args) != 1 {
