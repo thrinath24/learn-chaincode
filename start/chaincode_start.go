31c7bba0c8baa75cef157737515506365a703c25
thrinath24 <thrinathsai1995@gmail.com>
2017-03-10 11:33:53 +0530

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -87,6 +87,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	
 	fmt.Println("Welcome tothe  Supply chain management , Deployment has been started, do as u want")
 	fmt.Printf("Hope for best")
+	fmt.Printf("Plan for the worst")
  
        if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
